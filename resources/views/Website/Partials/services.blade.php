8c9889e72015ecf249047e58cebef841f976d0ec
seif <saifaboashour98@gmail.com>
2022-09-24 11:52:33 +0300

    feat: Services Content

--- resources/views/Website/Partials/services.blade.php
+++ resources/views/Website/Partials/services.blade.php
@@ -9,8 +9,8 @@
                         <div class="icon-bg"></div>
                         <img src="assets/img/icon/sr-3-1.png" alt="Service Shape">
                     </div>
-                    <h3 class="service-title h5"><a class="text-inherit" href="service-details.html">Start From Scratch To Business Care</a></h3>
-                    <p class="service-text">Plagiarize value added alignments via maintainable testing procedures. lie Energistically deploy leading.</p>
+                    <h3 class="service-title h5"><a class="text-inherit" href="service-details.html">Software Development</a></h3>
+                    <p class="service-text">Plan, Implement and Test softwares on any needed Platform (Web, Desktop, Mobile).</p>
                     <a href="service-details.html" class="icon-btn style2"><i class="fas fa-long-arrow-right"></i></a>
                 </div>
             </div>
@@ -21,8 +21,8 @@
                         <div class="icon-bg"></div>
                         <img src="assets/img/icon/sr-3-2.png" alt="Service Shape">
                     </div>
-                    <h3 class="service-title h5"><a class="text-inherit" href="service-details.html">Financial Strategy Consultant</a></h3>
-                    <p class="service-text">Plagiarize value added alignments via maintainable testing procedures. lie Energistically deploy leading.</p>
+                    <h3 class="service-title h5"><a class="text-inherit" href="service-details.html">Marketing</a></h3>
+                    <p class="service-text">Provide marketing solutions that match the business Needs either online or offline.</p>
                     <a href="service-details.html" class="icon-btn style2"><i class="fas fa-long-arrow-right"></i></a>
                 </div>
             </div>
@@ -33,12 +33,12 @@
                         <div class="icon-bg"></div>
                         <img src="assets/img/icon/sr-3-3.png" alt="Service Shape">
                     </div>
-                    <h3 class="service-title h5"><a class="text-inherit" href="service-details.html">Growth Marketing Consultant</a></h3>
-                    <p class="service-text">Plagiarize value added alignments via maintainable testing procedures. lie Energistically deploy leading.</p>
+                    <h3 class="service-title h5"><a class="text-inherit" href="service-details.html">Consultant</a></h3>
+                    <p class="service-text">Book a session or a meeting with an expert in business development.</p>
                     <a href="service-details.html" class="icon-btn style2"><i class="fas fa-long-arrow-right"></i></a>
                 </div>
             </div>
-            <div class="col-xl-4">
+            <!-- <div class="col-xl-4">
                 <div class="service-style3">
                     <div class="service-icon">
                         <div class="icon-shape"></div>
@@ -49,7 +49,7 @@
                     <p class="service-text">Plagiarize value added alignments via maintainable testing procedures. lie Energistically deploy leading.</p>
                     <a href="service-details.html" class="icon-btn style2"><i class="fas fa-long-arrow-right"></i></a>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
